cea42ddde271b692c0162b2b04bec3b624c3000e
lihao <504944122@qq.com>
2022-07-10 14:02:47 +0800

    .

--- ReadMe/Javascript/React/2.SimpleLoginWeb/app.js
+++ ReadMe/Javascript/React/2.SimpleLoginWeb/app.js
@@ -1,11 +1,35 @@
 class Contact extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            password :'hello',
+            authorization : false
+        };
+        this.authorize = this.authorize.bind(this);
+    }
+
+    authorize(e){
+        let password = e.target.querySelector('input[type="password"]').value;
+        let auth = password == this.state.password;
+        this.setState({authorization:auth});
+    }
+
     render(){
+
+        let login = <form action="#" onSubmit={this.authorize}>
+                        <input type="password" placeholder="password"></input>
+                        <input type="submit"></input>
+                    </form>;
+        
+        let contactInfo =  <ul>
+                                <li> client@example.com</li>
+                                <li> 555.555.5555 </li>
+                            </ul>
+        
+
         return (
-            <div>
-                <ul>
-                    <li> client@example.com</li>
-                    <li> 555.555.5555 </li>
-                </ul>
+            <div id="authorization">
+               <h1>{this.state.authorization? contactInfo : login}</h1>
             </div>
         );
     }
